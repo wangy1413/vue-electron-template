d620fee5cf6aa50f2ec0c1f264ab7e6e3260b4bc
wangy1413 <1580229949@qq.com>
2022-04-08 23:03:42 +0800

    Update Home.vue
    VIP-Video

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -2,27 +2,32 @@
   <div>
     <section class="hero is-dark is-bold">
       <div class="hero-body">
-        <br />
         <div class="block has-text-centered">
-          <figure class="image is-128x128">
-            <img alt="Placeholder image" src="../../../_icons/512pxblue.png" />
-          </figure>
-        </div>
-        <div class="content has-text-centered">
-          <h1 class="title is-4">
-            Vue-Electron-Template
+          <h1 class="title is-3" style="color: coral">
+            使用说明
           </h1>
-          <p class="subtitle is-6">
-            The boilerplate for making electron applications using vue.js.
+        </div>
+        <div class="content">
+          <p class="subtitle is-5" style="color:deepskyblue">
+            1.点击菜单栏【视频平台】，选择一个平台网站并等待进入页面。
           </p>
-          <RouterLink class="button is-primary" to="about">
-            <i class="material-icons">info</i>
-            <span>About</span>
-          </RouterLink>
-          <RouterLink class="button is-primary" to="help">
-            <i class="material-icons">help</i>
-            <span>Help</span>
-          </RouterLink>
+          <img class="image is-128x158" style="margin:0 auto" alt="Placeholder image" src="../../../_icons/platform.png" />
+
+          <p class="subtitle is-5" style="margin-top: 20px;color:deepskyblue">
+            2.进入某个视频平台网站，例如"腾讯视频"，搜索选择想看的视频节目。
+          </p>
+          <img class="image is-1333x870" style="margin:0 auto" alt="Placeholder image" src="../../../_icons/videoP.png" />
+
+          <p class="subtitle is-5" style="margin-top: 20px; color:deepskyblue">
+            3.当视频节目有广告或需要VIP时，可点击菜单栏【破解源】并选择一个。
+          </p>
+          <img class="image is-1333x870" style="margin:0 auto" alt="Placeholder image" src="../../../_icons/video.png" />
+          <img class="image is-1333x870" style="margin:0 auto" alt="Placeholder image" src="../../../_icons/crackS.png" />
+
+          <p class="subtitle is-5" style="margin-top: 20px; color:deepskyblue">
+            4.破解完成后，就可以爽快的观看视频了。
+          </p>
+          <img class="image is-1333x870" style="margin:0 auto" alt="Placeholder image" src="../../../_icons/crackVideo.png" />
         </div>
       </div>
     </section>
@@ -41,6 +46,6 @@ export default Vue.extend({
 
 <style>
 .hero-body {
-  height: 100vh;
+  height: 630vh;
 }
 </style>
